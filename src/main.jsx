371dce16cc275d15fe0a1d35b2c7644e2856ca12
Andrew Cain <acain89@gmail.com>
2025-11-14 20:09:21 -0600

    Include production env + update files

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
