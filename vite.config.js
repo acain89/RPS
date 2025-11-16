ca09d0298d19058daea47b0c9f39f49aa5d9251f
Andrew Cain <acain89@gmail.com>
2025-11-15 18:16:40 -0600

    Fix production backend URL

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  resolve: {
-    dedupe: ["react", "react-dom"],   // helps avoid duplicate React copies
-  },
-
-  optimizeDeps: {
-    include: ["react-router", "react-router-dom"],
-    exclude: ["@emotion/is-prop-valid"]  // stops require() emission
-  },
-
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true
-    },
-    rollupOptions: {
-      external: ["@emotion/is-prop-valid"]
-    }
-  }
 });
